b8da0ec1f96fd1b839a20fbf228992774d89ae12
Ziad ahmed <zaidahmed192007@gmail.com>
2026-05-04 18:42:19 +0300

    validating sign up page

--- Js/signup.js
+++ Js/signup.js
@@ -1,28 +1,79 @@
 //signup 
+$(function () {
+    $("#signup-form").validate({
+        rules: {
+            fullname: {
+                required: true,
+            },
+            username: {
+                required: true,
+
+            },
+            password: {
+                required: true,
+                minlength: 7
+            },
+
+            confirmPassword: {
+                required: true,
+                minlength: 7,
+                equalTo: "#password"
+            },
+            userEmail: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
 
 
-document.getElementById("signup-form").addEventListener("submit", (event) => {
+            username: {
+                required: "please enter your username",
 
-    event.preventDefault();
-    let name = document.getElementById("name").value;
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let confirmPassword = document.getElementById("confirmPassword").value;
-    let userEmail = document.getElementById("userEmail").value;
+            },
+            password: {
+                required: "please enter your password",
+                minlength: "your password must be at least 7 characters long"
+            },
 
-    if (password !== confirmPassword){
-        alert("passwords not match");
-        return;
-    }
-    const user = {
-        name: name,
-        username: username,
-        password: password,
-        userEmail: userEmail,
-    };
-    localStorage.setItem(userEmail, JSON.stringify(user));
-    alert("Registration successful! please login.");
-    window.location.href = "login.html";
+            confirmPassword: {
+                required: "please enter your password again",
+                minlength: "your password must be at least 7 characters long",
+                equalTo: "Both password and confirm password must be equal"
+            },
+            userEmail: {
+                required: "please enter a valid email address",
+
+            }
+        },
+        submitHandler: function (form, event) {
+            event.preventDefault();
+            let name = document.getElementById("name").value;
+            let username = document.getElementById("username").value;
+            let password = document.getElementById("password").value;
+            let confirmPassword = document.getElementById("confirmPassword").value;
+            let userEmail = document.getElementById("userEmail").value;
+
+
+
+
+            const user = {
+                name: name,
+                username: username,
+                password: password,
+                userEmail: userEmail,
+            };
+            localStorage.setItem(userEmail, JSON.stringify(user));
+            alert("Registration successful! please login.");
+            window.location.href = "login.html";
+            return false;
+        }
+    });
+
 });
+
+
+
+
 
 
